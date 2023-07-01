62d6d487a9170e1866d96b28710137b50e774656
Samuel Okello <samuel.okello@studentambassadors.com>
2023-07-01 12:32:13 +0300

    update heelo.java

--- Introduction/hello.java
+++ Introduction/hello.java
@@ -1,5 +1,5 @@
 public class hello{
     public static void main(String[] args) {
-        System.out.print("Hello World"+"\n"+"I am A java beginner");
+        System.out.print("Hello World"+"\n"+"I am A java beginner\n");
     }
 }
